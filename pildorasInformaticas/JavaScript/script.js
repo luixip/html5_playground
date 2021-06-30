377becc08b875902a09066e76f4cf4ea98897ef4
lesepulveda <39435246+lesrUTP@users.noreply.github.com>
2021-06-29 20:52:43 -0500

    Video 13 ok

--- pildorasInformaticas/JavaScript/script.js
+++ pildorasInformaticas/JavaScript/script.js
@@ -7,4 +7,21 @@ function saludo (){
     alert("Hola mundo");
 }
 
+function ejecuta (){
+    for (var i=0; i<3; i++) {
+        document.getElementsByTagName("p")[i].onclick=saludo;
+        // document.getElementById("verde").onclick=saludo;
+    }
+}
+
+function ejecuta2 (){
+    tope = document.getElementsByClassName("importante").length;
+    for (var i=0; i<tope; i++) {
+        var z=document.getElementsByClassName("importante")[i].onclick=saludo;
+    }
+}
+
+window.onload = ejecuta2;
+
+
 
